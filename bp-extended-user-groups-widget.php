9753109892b2db7a20c6d5e7ae0a018d931a8575
Brajesh Singh <brajesh@cosmiccoders.com>
2019-10-02 04:02:26 +0530

    cleanup the main file

--- bp-extended-user-groups-widget.php
+++ bp-extended-user-groups-widget.php
@@ -1,6 +1,6 @@
 <?php
-
-/* Plugin Name: BuddyPress Extended User Groups Widget
+/**
+ * Plugin Name: BuddyPress Extended User Groups Widget
  * Plugin URI: https://buddydev.com/plugins/bp-extended-user-groups-widget/
  * Version: 1.0.3
  * Description: Flexible group listing for BuddyPress user groups
@@ -8,13 +8,13 @@
  * Author URI: https://buddydev.com/
  * License: GPL
  *
- * */
+ **/
 
 /**
  * Contributor Name: Ravi Sharma, Brajesh Singh
  */
 
-// exit if file access directly
+// exit if file accessed directly.
 if ( ! defined( 'ABSPATH' ) ) {
 	exit;
 }
@@ -22,62 +22,79 @@ if ( ! defined( 'ABSPATH' ) ) {
 /**
  * Class BP_Extended_User_Groups_Widget_Helper
  */
-Class BP_Extended_User_Groups_Widget_Helper {
-    
-    private static $instance;
-    private $path;
-    
-    private function __construct() {
-
-        $this->path = plugin_dir_path( __FILE__ );
-        $this->setup();
-
-    }
-    
-    public static function get_instance() {
-
-        if ( ! isset( self::$instance ) ) {
-            self::$instance = new self();
-        }
-        return self::$instance;
-
-    }
-    
-    private function setup() {
-
-        add_action( 'bp_loaded', array( $this, 'load' ) );
-        add_action( 'bp_widgets_init', array( $this, 'register_widget' ), 10 );
-        add_action( 'bp_init', array( $this, 'load_text_domain' ) );
-
-    }
-
-    /**
-     * load widget class
-     */
-    public function load() {
-
-        require_once $this->path . 'class-bp-extended-user-groups-widget.php';
-
-    }
+class BP_Extended_User_Groups_Widget_Helper {
 
 	/**
-     * load language text domain
-     */
-    public function load_text_domain() {
+	 * Singeton instance.
+	 *
+	 * @var BP_Extended_User_Groups_Widget_Helper
+	 */
+	private static $instance;
 
-        load_plugin_textdomain( 'bp-extended-user-groups-widget', FALSE, plugin_basename( dirname( __FILE__ ) ) . '/languages' );
+	/**
+	 * Path to the plugin directory.
+	 *
+	 * @var string
+	 */
+	private $path;
 
-    }
+	/**
+	 * Constructor.
+	 */
+	private function __construct() {
 
-    public function register_widget() {
+		$this->path = plugin_dir_path( __FILE__ );
+		$this->setup();
 
-        register_widget( 'BP_Extended_User_Groups_Widget' );
+	}
 
-    }
+	/**
+	 * Singleton access method.
+	 *
+	 * @return BP_Extended_User_Groups_Widget_Helper
+	 */
+	public static function get_instance() {
+
+		if ( ! isset( self::$instance ) ) {
+			self::$instance = new self();
+		}
+
+		return self::$instance;
+
+	}
+
+	/**
+	 * Setup hooks.
+	 */
+	private function setup() {
+
+		add_action( 'bp_loaded', array( $this, 'load' ) );
+		add_action( 'bp_widgets_init', array( $this, 'register_widget' ), 10 );
+		add_action( 'bp_init', array( $this, 'load_text_domain' ) );
+
+	}
+
+	/**
+	 * Load widget class
+	 */
+	public function load() {
+		require_once $this->path . 'class-bp-extended-user-groups-widget.php';
+	}
+
+	/**
+	 * Load Localization files.
+	 */
+	public function load_text_domain() {
+		load_plugin_textdomain( 'bp-extended-user-groups-widget', false, plugin_basename( dirname( __FILE__ ) ) . '/languages' );
+	}
+
+	/**
+	 * Register widget.
+	 */
+	public function register_widget() {
+		register_widget( 'BP_Extended_User_Groups_Widget' );
+	}
 
 }
+
 BP_Extended_User_Groups_Widget_Helper::get_instance();
-
-
-
-
